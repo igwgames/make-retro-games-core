b885c1b1055f56509babbbafe01f3b90c4fbffff
cppchriscpp <admin@cpprograms.net>
2019-08-25 22:48:01 +0100

    Adding first templated variables

--- source/configuration/game_info.c
+++ source/configuration/game_info.c
@@ -1,6 +1,15 @@
 // A bunch of information about your game - used on the title screen, as well as 
 // the credits, and anywhere else you fancy!
-const unsigned char gameName[] = "Quirk's Adventure";
-const unsigned char gameAuthor[] = "YOU!";
-const unsigned char gameAuthorContact[] = "Tweet at us! @your_twitter";
-const unsigned char currentYear[] = "2018";
+
+// ----- RETRO INTERACTIVE CODE REPLACEMENT ----
+// ----- DO NOT EDIT BELOW THIS LINE -----
+#define RI_EDITOR_GAME_NAME "RI Demo Game"
+#define RI_EDITOR_GAME_AUTHOR "YOU!"
+#define RI_GAME_AUTHOR_CONTACT "Tweet at us! @your_twitter"
+#define RI_GAME_YEAR "2019"
+// ----- END RETRO INTERACTIVE CODE REPLACEMENT ---- 
+
+const unsigned char gameName[] = RI_EDITOR_GAME_NAME;
+const unsigned char gameAuthor[] = RI_EDITOR_GAME_AUTHOR;
+const unsigned char gameAuthorContact[] = RI_GAME_AUTHOR_CONTACT;
+const unsigned char currentYear[] = RI_GAME_YEAR;
