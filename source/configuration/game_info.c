e7cf6b3a31fa479c33f092d801f17232167bd9f2
cppchriscpp <admin@cpprograms.net>
2019-09-03 19:16:05 -0400

    Typo fix

--- source/configuration/game_info.c
+++ source/configuration/game_info.c
@@ -1,13 +1,13 @@
 // A bunch of information about your game - used on the title screen, as well as 
 // the credits, and anywhere else you fancy!
 
-// ----- RETRO INTERACTIVE CODE REPLACEMENT ----
+// ----- RETRO INTERACTIVE CODE REPLACEMENT -----
 // ----- DO NOT EDIT BELOW THIS LINE -----
 #define RI_EDITOR_GAME_NAME "RI Demo Game"
 #define RI_EDITOR_GAME_AUTHOR "YOU!"
 #define RI_GAME_AUTHOR_CONTACT "Tweet at us! @your_twitter"
 #define RI_GAME_YEAR "2019"
-// ----- END RETRO INTERACTIVE CODE REPLACEMENT ---- 
+// ----- END RETRO INTERACTIVE CODE REPLACEMENT ----- 
 
 const unsigned char gameName[] = RI_EDITOR_GAME_NAME;
 const unsigned char gameAuthor[] = RI_EDITOR_GAME_AUTHOR;
