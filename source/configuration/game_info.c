18d63a6f40a04ac11f6baef4baa10253b1a0f65f
cppchriscpp <admin@cpprograms.net>
2019-09-08 17:27:18 +0100

    fixing minor issues

--- source/configuration/game_info.c
+++ source/configuration/game_info.c
@@ -1,14 +1,6 @@
 // A bunch of information about your game - used on the title screen, as well as 
 // the credits, and anywhere else you fancy!
-
-// ----- RETRO INTERACTIVE CODE REPLACEMENT -----
-// ----- DO NOT EDIT BELOW THIS LINE -----
-    #define RI_EDITOR_GAME_NAME "RI Demo Game Too??"
-    #define RI_EDITOR_GAME_AUTHOR "YOU!"
-    #define RI_GAME_AUTHOR_CONTACT "Tweet at us! @your_twitter"
-    #define RI_GAME_YEAR "2019"
-    #define RI_DEBUG_MODE_BOOL "0"
-// ----- END RETRO INTERACTIVE CODE REPLACEMENT ----- 
+#include "source/configuration/game_info.h"
 
 const unsigned char gameName[] = RI_EDITOR_GAME_NAME;
 const unsigned char gameAuthor[] = RI_EDITOR_GAME_AUTHOR;
--- source/configuration/game_info.h
+++ source/configuration/game_info.h
@@ -1,6 +1,16 @@
 // A bunch of information about your game - used on the title screen, as well as 
 // the credits, and anywhere else you fancy!
 // Values are written in game_info.c
+
+// ----- RETRO INTERACTIVE CODE REPLACEMENT -----
+// ----- DO NOT EDIT BELOW THIS LINE -----
+    #define RI_EDITOR_GAME_NAME "RI Demo Game Too??"
+    #define RI_EDITOR_GAME_AUTHOR "YOU!"
+    #define RI_GAME_AUTHOR_CONTACT "Tweet at us! @your_twitter"
+    #define RI_GAME_YEAR "2019"
+    #define RI_DEBUG_MODE_BOOL 0
+// ----- END RETRO INTERACTIVE CODE REPLACEMENT ----- 
+
 extern const unsigned char gameName[];
 extern const unsigned char gameAuthor[];
 extern const unsigned char gameAuthorContact[];
