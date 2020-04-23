64df9ca33983e2d41bdb72c83988426fa0105460
cppchriscpp <admin@cpprograms.net>
2020-04-24 00:28:31 +0100

    Attempt a github action

---
+++ tools/github-action-build/Dockerfile
@@ -0,0 +1,6 @@
+FROM cppchriscpp/cc65-node:latest
+
+COPY ../.. /
+COPY ./build.sh /
+
+ENTRYPOINT ["/build.sh"]
---
+++ tools/github-action-build/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -l
+
+# Get dependencies for all npm-based tools
+cd tools/tmx2c/src && npm install && cd ../../../tools/sprite_def2img/src && npm install && cd ../../../tools/chr2img/src && npm install
+
+make
