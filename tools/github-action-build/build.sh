167952b17516455e8ed60a806c4cb53b634f7b7c
cppchriscpp <admin@cpprograms.net>
2020-04-24 02:06:51 +0100

    Where are my files?

--- tools/github-action-build/build.sh
+++ tools/github-action-build/build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh -l
 
-# Get dependencies for all npm-based tools
-# cd tools/tmx2c/src && npm install && cd ../../../tools/sprite_def2img/src && npm install && cd ../../../tools/chr2img/src && npm install
-
 cd /
-ls -lah tools/tmx2c/src
 CI_BUILD=true make
+ls -lah /rom
