2c0c336273ada6830af2da946f952bf83f385ba8
cppchriscpp <admin@cpprograms.net>
2020-04-24 01:41:26 +0100

    Right, docker is weird

--- tools/github-action-build/build.sh
+++ tools/github-action-build/build.sh
@@ -1,8 +1,4 @@
 #!/bin/sh -l
 
-# Get dependencies for all npm-based tools
-cd tools/tmx2c/src && npm install && cd ../../../tools/sprite_def2img/src && npm install && cd ../../../tools/chr2img/src && npm install
-
-cd /
-ls -lah tools/tmx2c/src/node_modules
-CI_BUILD=true make
+# Get dependencies for all npm-based tools, and build all at once.
+cd tools/tmx2c/src && npm install && cd ../../../tools/sprite_def2img/src && npm install && cd ../../../tools/chr2img/src && npm install && cd / && CI_BUILD=true make
