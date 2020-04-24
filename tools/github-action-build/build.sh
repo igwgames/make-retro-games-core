0254230df5d32fcd1b1e965e5fabdde71e0210fa
cppchriscpp <admin@cpprograms.net>
2020-04-24 03:14:53 +0100

    Suppose I needed to have that dir there

--- tools/github-action-build/build.sh
+++ tools/github-action-build/build.sh
@@ -4,6 +4,7 @@ cd /
 CI_BUILD=true make
 ls -lah /rom
 ls -lah /github/home
-cp rom/* $GITHUB_WORKDSPACE
+# cp rom/* $GITHUB_WORKDSPACE
+mkdir /github/home/rom || echo "Dir exists"
 cp rom/starter.nes /github/home/rom
 ls -lah /github/home/rom
