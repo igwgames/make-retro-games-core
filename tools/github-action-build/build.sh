b51e1013e1c4951cc22d33023b7a25fb73980893
cppchriscpp <admin@cpprograms.net>
2020-04-25 06:24:14 +0100

    Lets just keep a separate collision map entry for each tile. Makes code simpler everywhere

--- tools/github-action-build/build.sh
+++ tools/github-action-build/build.sh
@@ -2,9 +2,6 @@
 
 cd /
 CI_BUILD=true make
-ls -lah /rom
-ls -lah /github/home
-# cp rom/* $GITHUB_WORKSPACE
+
 mkdir $GITHUB_WORKSPACE/rom || echo "Dir exists"
-cp rom/starter.nes $GITHUB_WORKSPACE/rom/starter.nes
-ls -lahr $GITHUB_WORKSPACE/rom
+cp rom/starter.nes $GITHUB_WORKSPACE/rom/game.nes
