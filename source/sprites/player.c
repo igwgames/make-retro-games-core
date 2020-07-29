046905ee4db1a400b6d84d1ab1a2f79aaffd8fb3
cppchriscpp <admin@cpprograms.net>
2020-07-29 01:21:13 +0100

    Reduce frequency of grid adjustments

--- source/sprites/player.c
+++ source/sprites/player.c
@@ -207,20 +207,22 @@ void handle_player_movement(void) {
         }
 
         // Now, adjust to the grid as possible, if the player isn't pressing a direction.
-        if (playerYVelocity != 0 && playerXVelocity == 0 && (controllerState & (PAD_LEFT | PAD_RIGHT)) == 0) {
-            if ((char)((playerXPosition + PLAYER_POSITION_TILE_MASK_MIDDLE) & PLAYER_POSITION_TILE_MASK) > (char)(PLAYER_POSITION_TILE_MASK_MIDDLE)) {
-                playerXVelocity = 0-PLAYER_VELOCITY_NUDGE;
-            } else if ((char)((playerXPosition + PLAYER_POSITION_TILE_MASK_MIDDLE) & PLAYER_POSITION_TILE_MASK) < (char)(PLAYER_POSITION_TILE_MASK_MIDDLE)) {
-                playerXVelocity = PLAYER_VELOCITY_NUDGE;
-            } // If equal, do nothing. That's our goal.
-        } 
+        if (frameCount & 0x01) {// Do this every other frame, to make it more subtle
+            if (playerYVelocity != 0 && playerXVelocity == 0 && (controllerState & (PAD_LEFT | PAD_RIGHT)) == 0) {
+                if ((char)((playerXPosition + PLAYER_POSITION_TILE_MASK_MIDDLE) & PLAYER_POSITION_TILE_MASK) > (char)(PLAYER_POSITION_TILE_MASK_MIDDLE)) {
+                    playerXVelocity = 0-PLAYER_VELOCITY_NUDGE;
+                } else if ((char)((playerXPosition + PLAYER_POSITION_TILE_MASK_MIDDLE) & PLAYER_POSITION_TILE_MASK) < (char)(PLAYER_POSITION_TILE_MASK_MIDDLE)) {
+                    playerXVelocity = PLAYER_VELOCITY_NUDGE;
+                } // If equal, do nothing. That's our goal.
+            } 
 
-        if (playerXVelocity != 0 && playerYVelocity == 0 && (controllerState & (PAD_UP | PAD_DOWN)) == 0) {
-            if ((char)((playerYPosition + PLAYER_POSITION_TILE_MASK_MIDDLE + PLAYER_POSITION_TILE_MASK_EXTRA_NUDGE) & PLAYER_POSITION_TILE_MASK) > (char)(PLAYER_POSITION_TILE_MASK_MIDDLE)) {
-                playerYVelocity = 0-PLAYER_VELOCITY_NUDGE;
-            } else if ((char)((playerYPosition + PLAYER_POSITION_TILE_MASK_MIDDLE + PLAYER_POSITION_TILE_MASK_EXTRA_NUDGE) & PLAYER_POSITION_TILE_MASK) < (char)(PLAYER_POSITION_TILE_MASK_MIDDLE)) {
-                playerYVelocity = PLAYER_VELOCITY_NUDGE;
-            } // If equal, do nothing. That's our goal.
+            if (playerXVelocity != 0 && playerYVelocity == 0 && (controllerState & (PAD_UP | PAD_DOWN)) == 0) {
+                if ((char)((playerYPosition + PLAYER_POSITION_TILE_MASK_MIDDLE + PLAYER_POSITION_TILE_MASK_EXTRA_NUDGE) & PLAYER_POSITION_TILE_MASK) > (char)(PLAYER_POSITION_TILE_MASK_MIDDLE)) {
+                    playerYVelocity = 0-PLAYER_VELOCITY_NUDGE;
+                } else if ((char)((playerYPosition + PLAYER_POSITION_TILE_MASK_MIDDLE + PLAYER_POSITION_TILE_MASK_EXTRA_NUDGE) & PLAYER_POSITION_TILE_MASK) < (char)(PLAYER_POSITION_TILE_MASK_MIDDLE)) {
+                    playerYVelocity = PLAYER_VELOCITY_NUDGE;
+                } // If equal, do nothing. That's our goal.
+            }
         }
     }
 
